33b48c01b69bc53a994b7f79a447eb1668e7cbc3
Divy Srivastava <me@littledivy.com>
2025-09-25 22:27:31 +0530

    add WKD for pgp key

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ import { serveDir, serveFile } from "jsr:@std/http/file-server";
 Deno.serve(async (req) => {
   const res = await serveDir(req, {
     fsRoot: "dist",
+    showDotfiles: true,
   });
   if (res.status == 404) {
     return serveFile(req, `dist/${new URL(req.url).pathname}.html`);
